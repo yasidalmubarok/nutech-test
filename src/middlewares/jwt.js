d427c2220822889ba325a4f5909d78b7df4a1951
unknown <yasidalmubarok@gmail.com>
2025-11-02 15:47:06 +0700

    edit: expiration token time

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -27,6 +27,6 @@ export const generateToken = (user) => {
     return jwt.sign(
         { id: user.id, email: user.email },
         process.env.JWT_SECRET,
-        { expiresIn: process.env.JWT_EXPIRES_IN || '1h' }
+        { expiresIn: process.env.JWT_EXPIRES_IN || '12h' }
     );
 };
